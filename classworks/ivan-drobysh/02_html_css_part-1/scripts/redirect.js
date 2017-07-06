84dbdc5e4b99997b43a764a31d0b5ee145e03b8b
Ivan <v.drobysh2011@gmail.com>
2017-07-06 19:17:29 +0300

    Create Html page with Css

---
+++ classworks/ivan-drobysh/02_html_css_part-1/scripts/redirect.js
@@ -0,0 +1,29 @@
+function onGitHub(){
+  document.location.href = "http://github.com";
+}
+function onFacebook(){
+  document.location.href = "http://facebook.com";
+}
+function onDribbble(){
+  document.location.href = "https://dribbble.com";
+}
+function onTwitter(){
+  document.location.href = "http://twitter.com";
+}
+function onMail(){
+  document.location.href = "http://mail.google.com";
+}
+
+function onIntro(){
+  document.location.href = "#Intro";
+}
+function onPortfolio(){
+  alert("d");
+  document.location.href = "#Intro";
+}
+function onAbout(){
+  document.location.href = "#Intro";
+}
+function onContact(){
+  document.location.href = "#Intro";
+}
