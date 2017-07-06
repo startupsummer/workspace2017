7a2f07463139c3914f23b15a6b73745967b29480
Nataliya Karatkova <nataliyakaratkova@gmail.com>
2017-07-07 02:58:13 +0300

    HTML & CSS lecture part2: Update task

---
+++ lectures/03_html_css_part-2/task/boilerplate/webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/prototype/assets/styles/index.pcss`,
+
+  output: {
+    path: `${__dirname}/build/`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.pcss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+          ],
+        }),
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 4,
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3,
+              },
+            },
+          }
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  devServer: {
+    hot: true,
+    contentBase: `${__dirname}/`,
+    publicPath: '/',
+  },
+
+  devtool: 'source-map',
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: './prototype/index.html', filename: 'index.html' }),
+    new ExtractTextPlugin('style.css'),
+  ],
+};
