66c035d7ebbfb29aa6c734a12ef66b9e08c5d9d7
Ivan <v.drobysh2011@gmail.com>
2017-07-10 14:43:13 +0300

    comlete task 4th lectur

---
+++ classworks/ivan-drobysh/04_webpack/starter/webpack.config.js
@@ -0,0 +1,54 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+            },
+            {
+              test: /\.(png|jpg|gif)$/,
+              use: [
+                {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 8192
+                  }
+                }
+              ]
+            },
+            {
+              test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['env']
+                }
+              }
+            },
+            {
+              test: /\.css$/,
+              use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+              })
+            }
+            ]
+    },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new UglifyJSPlugin()
+  ]
+}
