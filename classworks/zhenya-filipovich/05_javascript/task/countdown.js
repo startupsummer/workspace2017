900cc3a83d0e399109637ef66bd26361fd50521c
filipochka97 <zhenya.filippovich@gmail.com>
2017-07-11 13:59:32 +0300

    Implement this wonderfull  task

--- classworks/zhenya-filipovich/05_javascript/task/countdown.js
+++ classworks/zhenya-filipovich/05_javascript/task/countdown.js
@@ -2,21 +2,21 @@ const timer = document.querySelector('.timer');
 const table = document.querySelector('.data-results');
 
 const buttons = {
-  start: document.querySelector('.start.button'), 
+  start: document.querySelector('.start.button'),
   stop: document.querySelector('.stop.button'),
   reset: document.querySelector('.reset.button'),
   plus: document.querySelector('.plus.button'),
-  minus: document.querySelector('.minus.button'),
-  screen: document.querySelector('.screen.button'),
+	minus: document.querySelector('.minus.button'),
+	screen: document.querySelector('.screen.button'),
 };
 
 let startID;
-let count = 0;
+let count = 3600;
 let number = 0;
 let currentTime = '';
 let timerWorks = false; 
 
-timer.textContent = '00:00';
+timer.textContent = '60:00';
 
 buttons.start.addEventListener('click', startTimer);
 buttons.stop.addEventListener('click', stopTimer);
@@ -28,12 +28,12 @@ buttons.screen.addEventListener('click', addToTable);
 function startTimer() {
   if (timerWorks) return;
   const run = () => {
-    const date = new Date(++count * 1000);
+    const date = new Date(--count * 1000);
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     const currentTime = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-    
-    timer.textContent = currentTime;
+
+		timer.textContent = currentTime;
     startID = setTimeout(run, 1000);
   }
 
@@ -50,16 +50,18 @@ function resetTimer() {
   clearTimeout(startID);
   timerWorks = false;
   count = 0;
-  timer.textContent = '00:00';
+  timer.textContent = '60:00';
 }
 
 function plusTime() {
-  count += 10;
+	if (count < 3590) {
+		count += 10;
+	}
   startTimer();
 }
 
 function minusTime() {
-  if (count > 10) {
+  if (count > 100) {
     count -= 10;
   }
 }
@@ -72,8 +74,3 @@ function addToTable() {
       </div>`
   );
 }
-
-
-
-
-
