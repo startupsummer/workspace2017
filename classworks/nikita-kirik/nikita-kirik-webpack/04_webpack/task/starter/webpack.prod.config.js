19fc0ad4c0e36efc6c5f1749a5aa8aebdfc438de
Kirik Nikita <luis-gusman@yandex.ru>
2017-07-11 11:09:01 +0300

    implement task with advanced

---
+++ classworks/nikita-kirik/nikita-kirik-webpack/04_webpack/task/starter/webpack.prod.config.js
@@ -0,0 +1,39 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+
+ entry: `${__dirname}/index.js`,
+
+  output: {
+    path: `${__dirname}/build/`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+
+ module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+
+      {
+        test: /\.js$/,
+        loaders: [
+          'babel-loader',
+        ],
+      },
+  ]
+},
+
+
+
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+  ],
+
+}
