0f107ad4a8aa541258c1e801acdb3c42acb3fe9f
danilyanich <danilyanich@gmail.com>
2017-07-12 15:12:26 +0300

    [+] Promises and async.

--- classworks/dan-krachkouski/06_es6/index.js
+++ classworks/dan-krachkouski/06_es6/index.js
@@ -26,3 +26,28 @@ for (const item of iterator) {
 }
 
 console.log(`size: ${queue.length}`);
+
+function resolveAfter(value, ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(value), ms);
+  });
+}
+
+console.log('Promises started');
+
+Promise.all([1, 2, 3, 4, 5]
+  .map(i => resolveAfter(i, 1000 * i)))
+.then((values) => {
+  console.log(`Promises says: ${values}`);
+});
+
+console.log('Async started');
+
+async function sameButAsync() {
+  const one = await resolveAfter(1, 1000);
+  const two = await resolveAfter(2, 2000);
+  const three = await resolveAfter(3, 3000);
+  console.log('Async says: ', one, two, three);
+}
+
+sameButAsync(); // not the same
