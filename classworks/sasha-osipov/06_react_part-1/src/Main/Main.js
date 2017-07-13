140b37bcafee75005ecfd978ea88b94110f445b6
Osipov Sasha <fusionboss96@gmail.com>
2017-07-13 17:47:39 +0300

    add icons

--- classworks/sasha-osipov/06_react_part-1/src/Main/Main.js
+++ classworks/sasha-osipov/06_react_part-1/src/Main/Main.js
@@ -48,6 +48,11 @@ class Main extends Component {
     })
   }
 
+  searchTitle = (event) => {
+    const issuesValue = event.target.value;
+    console.log(issuesValue);
+  }
+
   render() {
 
   const renderIssues = this.state.issues.filter(item => item.state === this.state.tab);
@@ -84,6 +89,10 @@ class Main extends Component {
       <div className="issues-listing">
         <div className="issues-listing__subnav">
           <div className="subnav">
+            <form className="subnav__search">
+              <input className="subnav__search-input" type="text" placeholder="Search" onChange={this.searchTitle}/>
+              <svg className="subnav__search-icon" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fillRule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0 0 13 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 0 0 0-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"></path></svg>
+            </form>
             <Button btn primary text="New Issue" click={this.newIssue}/>
           </div>
         </div>
@@ -102,6 +111,14 @@ class Main extends Component {
           <ul className="issues">
               {renderIssues.length !== 0 ? renderIssues.map(item => 
                 <li key={ item.id } className="issues__item">
+                  { item.state === 'open' ? 
+                  <div className="issues__status issues__status--open">
+                    <svg className="issues__icon" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
+                  </div> : 
+                  <div className="issues__status issues__status--closed">
+                    <svg className="issues__icon" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
+                  </div> 
+                  }  
                   <div className="issues__title">
                     <a href="" className="issues__link">
                       {item.title}
