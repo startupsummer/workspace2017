3bc6695bea615696d9b86201766bb3e6c779c975
Ilya <i.kohanvs@gmail.com>
2017-07-13 17:09:52 +0300

    Really finish 07react

--- classworks/ilya-koanovskiy/07-react_intro/my-app/src/components/issuesList/index.js
+++ classworks/ilya-koanovskiy/07-react_intro/my-app/src/components/issuesList/index.js
@@ -10,10 +10,11 @@ class IssuesList extends Component{
     }
 
     createIssues = (issues) => {
-       return issues.filter((item)=>item.state == this.props.button);
+        if(this.props.button == "all") return issues;
+        return issues.filter((item)=>item.state == this.props.button);
     }
 
-    handleClick = (id) => () =>{
+    handleClick = (id) => () => {
         const newArray= this.props.info.map((item)=>{
             if(item.id == id) {
                 item.state = "closed";
@@ -27,25 +28,28 @@ class IssuesList extends Component{
 
     render(){
         return(
-                    <div className="issues-listing__body">
-                        <ul className="issues">
-                            {
-                            this.createIssues(this.props.info).map((item)=>                
-                                    <li key={item.id} className="issues__item">
-                                        <div className="issues__status issues__status--open">
-                                            <svg className="issues__icon" height="16" version="1.1" viewBox="0 0 14 16" width="14">
-                                                <path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path>
-                                            </svg>
-                                        </div>
-                                        <div className="issus__title">
-                                            <a className="issues__link">{item.title}</a>
-                                        </div>
-                                        {item.state === "open" && <Button class="btn issue__close" text="Close issue" onClick={this.handleClick(item.id)}/> }
-                                    </li>  
-                                    )                                          
-                            }
-                        </ul>
-                    </div>
+            <div className="issues-listing__body">
+                <ul className="issues">
+                    {
+                    this.createIssues(this.props.info).map((item)=>                
+                        <li key={item.id} className="issues__item">
+                            <div className="issues__statusAndTitle">
+                                <div className="issues__status issues__status--open">
+                                    <svg className="issues__icon" height="16" version="1.1" viewBox="0 0 14 16" width="14">
+                                        <path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path>
+                                    </svg>
+                                </div>
+                               
+                                <div className="issus__title">
+                                    <a className="issues__link">{item.title}</a>
+                                </div>
+                            </div>     
+                                {item.state === "open" && <Button class="btn issue__close" text="Close issue" onClick={this.handleClick(item.id)}/> }
+                        </li>  
+                        )                                          
+                    }
+                </ul>
+            </div>
         )
     }
 }
