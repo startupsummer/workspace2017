9f15d297a962f988f130ab1a54804e17450e952c
Natallia Lukyaniuk <nlukyaniuk@gmail.com>
2017-07-14 15:44:59 +0300

    add stateless components

--- classworks/natallia-lukyaniuk/08_react_introduction_part_2/src/Issue/Issue.js
+++ classworks/natallia-lukyaniuk/08_react_introduction_part_2/src/Issue/Issue.js
@@ -3,29 +3,24 @@ import { Link } from 'react-router-dom';
 import OpenIssueIcon from './OpenIssueIcon';
 import ClosedIssueIcon from './ClosedIssueIcon'
 
-class Issue extends Component {
-  render() {
-    const {issue, handleChangeState, status} = this.props;
-    return (
-      <div className="issues__item">
-        <div className="issues__status issues__status--open">
-          {
-              status === "open" ? <OpenIssueIcon /> : <ClosedIssueIcon />
-          }
-        </div>
-        <div className="issues__title">
-          <Link className="issues__link" to={`/${issue.id}`}>
-            {issue.title}
-          </Link>
-        </div>
-          {
-            status === "open"
-            ? <button className="btn issue__close" value="closed" type="button" onClick={handleChangeState.bind(null, issue.id)}> Close issue </button>
-            : <button className="btn issue__close" value="open" type="button" onClick={handleChangeState.bind(null, issue.id)}> Open issue </button>
-          }
-      </div>
-    )
-  }
-}
+const Issue = ({issue, handleChangeState, status}) => (
+  <div className="issues__item">
+    <div className="issues__status issues__status--open">
+      {
+          status === "open" ? <OpenIssueIcon /> : <ClosedIssueIcon />
+      }
+    </div>
+    <div className="issues__title">
+      <Link className="issues__link" to={`/${issue.id}`}>
+        {issue.title}
+      </Link>
+    </div>
+      {
+        status === "open"
+        ? <button className="btn issue__close" value="closed" type="button" onClick={handleChangeState.bind(null, issue.id)}> Close issue </button>
+        : <button className="btn issue__close" value="open" type="button" onClick={handleChangeState.bind(null, issue.id)}> Open issue </button>
+      }
+  </div>
+)
 
 export default Issue;
--- classworks/natallia-lukyaniuk/08_react_introduction_part_2/src/IssuesList/IssuesListContainer.js
+++ classworks/natallia-lukyaniuk/08_react_introduction_part_2/src/IssuesList/IssuesListContainer.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import Issue from '../Issue/Issue.js';
 
-class IssuesListContainer extends Component {
-  render() {
-    const {issues, issuesState, handleChangeState, selectIssue} = this.props;
-    return (
-      <div className="issues-listing__body">
-        <ul className="issues">
-          {
-            issues.filter((issue) => issue.state === issuesState).map((issue) => 
-              (
-                <li>
-                  <Issue issue={issue} status={issuesState} handleChangeState={handleChangeState} />
-                </li>
-              )
-            )
-          }
-        </ul>
-      </div>
-    )
-  }
-}
-
+const IssuesListContainer = ({issues, issuesState, handleChangeState, selectIssue}) => (
+  <div className="issues-listing__body">
+    <ul className="issues">
+      {
+        issues.filter((issue) => issue.state === issuesState).map((issue) => 
+          (
+            <li>
+              <Issue issue={issue} status={issuesState} handleChangeState={handleChangeState} />
+            </li>
+          )
+        )
+      }
+    </ul>
+  </div>
+)
 export default IssuesListContainer;
