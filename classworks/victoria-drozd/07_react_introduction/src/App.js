20511d6106b57c5d3a6de021b57bfa296a596c97
andemerie <imvictoriadrozd@gmail.com>
2017-07-14 12:08:29 +0300

    (07_react_introduction): remove close issue button from closed tab

--- classworks/victoria-drozd/07_react_introduction/src/App.js
+++ classworks/victoria-drozd/07_react_introduction/src/App.js
@@ -131,13 +131,14 @@ class Header extends Component {
 
 class Item extends Component {
   render() {
-    let svg, classStr = "issues__status";
+    let svg, classStr = "issues__status", closeIssueButton;
     if (this.props.currentTab === 'open') {
       svg =
         <svg className="issues__icon" height={16} version="1.1" viewBox="0 0 14 16" width={16}>
           <path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"/>
         </svg>;
       classStr += " issues__status--open";
+      closeIssueButton = <Btn classes="btn issue__close" text="Close issue" />
     } else if (this.props.currentTab === 'closed') {
       svg =
         <svg className="issues__icon" aria-hidden="true" height={16} version="1.1" viewBox="0 0 16 16" width={16}>
@@ -156,7 +157,7 @@ class Item extends Component {
             {this.props.title}
           </a>
         </div>
-        <Btn classes="btn issue__close" text="Close issue" />
+        {closeIssueButton}
       </li>
     );
   }
