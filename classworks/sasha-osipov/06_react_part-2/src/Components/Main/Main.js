228358d7d80e82ff63c4b99cfda4ed9f2990b45c
Osipov Sasha <fusionboss96@gmail.com>
2017-07-17 16:20:16 +0300

    add advanced task

--- classworks/sasha-osipov/06_react_part-2/src/Components/Main/Main.js
+++ classworks/sasha-osipov/06_react_part-2/src/Components/Main/Main.js
@@ -3,7 +3,6 @@ import './Main.css';
 import Container from '../Container/Container';
 import Pagehead from '../Pagehead/Pagehead';
 import IssuesListing from '../IssuesListing/IssuesListing';
-import data from '../../data';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Description from '../Description/Description';
 
@@ -11,10 +10,27 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            issues: data,
+            issues: [],
             tab: 'open',
             value: '',
         }
+    }
+
+    componentDidMount() {
+        fetch('https://api.github.com/repos/Fusionqq/GitHub-Issues/issues?access_token=39b27afcbfd15dda6d0cd88c02936095fb462ee3&state=all')
+        .then(response => response.json())
+        .then((data) => {
+            const issuesList = data.map(item => ({
+                id: item.id,
+                description: item.body,
+                title: item.title,
+                state: item.state,
+                number: item.number,
+            }));
+            this.setState({
+                issues: issuesList,
+            })
+        }) 
     }
 
     openList = () => {
@@ -30,30 +46,57 @@ class Main extends Component {
     }
 
     openItem = (id) => () => {
-        this.setState({
-            issues: this.state.issues.map(item => item.id === id ? { ...item, state: 'open'} : item),
+        const issue = this.state.issues.filter(item => item.id === id)[0]
+
+        fetch(`https://api.github.com/repos/Fusionqq/GitHub-Issues/issues/${issue.number}?access_token=39b27afcbfd15dda6d0cd88c02936095fb462ee3`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                state: 'open',
+            })
+        })
+        .then(response => response.json())
+        .then(() => {
+            this.setState({
+                issues: this.state.issues.map(item => item.id === id ? { ...item, state: 'open'} : item),
+            })
         })
     }
 
     closeItem = (id) => () => {
-        this.setState({
-            issues: this.state.issues.map(item => item.id === id ? { ...item, state: 'closed'} : item),
+        const issue = this.state.issues.filter(item => item.id === id)[0]
+
+        fetch(`https://api.github.com/repos/Fusionqq/GitHub-Issues/issues/${issue.number}?access_token=39b27afcbfd15dda6d0cd88c02936095fb462ee3`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                state: 'closed',
+            })
+        })
+        .then(response => response.json())
+        .then(() => {
+            this.setState({
+                issues: this.state.issues.map(item => item.id === id ? { ...item, state: 'closed'} : item),
+            })
         })
     }
 
     newIssue = () => {
         const newItem = {
-            id: Math.random()*1000000000,
             state : "open",
             title : "Hello, it's me",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel su
-                    scipit leo. Mauris ultricies, leo sit amet accumsan ultricies, arcu 
-                    ipsum tempus libero, non tincidunt arcu est ac elit.`,
-        };
-        const newIssues = [...this.state.issues, newItem];
-        this.setState({
-            issues: newIssues,
+            body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel suscipit leo. Mauris ultricies, leo sit amet accumsan ultricies, arcu ipsum tempus libero, non tincidunt arcu est ac elit.`,
+        }
+
+        fetch(`https://api.github.com/repos/Fusionqq/GitHub-Issues/issues?access_token=39b27afcbfd15dda6d0cd88c02936095fb462ee3`, {
+            method: 'POST',
+            body: JSON.stringify(newItem),
         })
+        .then(response => response.json())
+        .then((data) => {
+            const newIssues = [...this.state.issues, data];
+            this.setState({
+                issues: newIssues,
+            })
+        }) 
     }
 
     searchTitle = (event) => {
@@ -63,7 +106,6 @@ class Main extends Component {
     }
 
     render() {
-       
         return (
             <Router>
                 <main className="content">
