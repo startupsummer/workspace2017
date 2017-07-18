2ec640682360eb05b52c3da732ac2d56e152673c
Chepicov Mark <chepicov.mark@mail.ru>
2017-07-18 17:33:32 +0300

    Update items-reducer.js

--- classworks/mark-chepicov/react-redux/my-app/src/resources/items/items-reducer.js
+++ classworks/mark-chepicov/react-redux/my-app/src/resources/items/items-reducer.js
@@ -1,9 +1,9 @@
 export default (state = [], action) => {
   switch (action.type) {
     case 'fetchItems':
-      return action.payload.map(item => {return {...item, id: `${item.id}`}});
+      return action.payload.map(item => {return {...item, id: item.id.toString()}});
     case 'itemAdd':
-      return [...state, action.payload];
+      return [...state, {...action.payload, id: action.payload.id.toString()}];
     case 'itemOpen':
       return state.map(issue =>
         issue.id === action.id ? { ...issue, state: 'open' }
