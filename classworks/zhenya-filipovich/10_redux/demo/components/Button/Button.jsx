19db5ca4c3db3e52a42490c650f32c8f518398bd
filipochka97 <zhenya.filippovich@gmail.com>
2017-07-19 07:04:43 +0300

    Fix some problems with eslint

---
+++ classworks/zhenya-filipovich/10_redux/demo/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './button.styles';
+
+
+const Button = ({ onClick, title, small }) => (
+  <div className={cx('button', { 'button--small': small })} onClick={onClick}>
+    {title}
+  </div>
+);
+
+Button.propsTypes = {
+  onClick: PropTypes.func.isRequired,
+
+  title: PropTypes.object.isRequired,
+  small: PropTypes.bool,
+};
+
+export default Button;
