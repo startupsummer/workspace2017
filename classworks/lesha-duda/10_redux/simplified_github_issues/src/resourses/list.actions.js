615da1019d0cea8c056fc2c026b5c3441cd28f35
Duda, Aleksey <dudaaleksey@gmail.com>
2017-07-19 12:48:22 +0300

    Resource files

---
+++ classworks/lesha-duda/10_redux/simplified_github_issues/src/resourses/list.actions.js
@@ -0,0 +1,18 @@
+import * as listApi from "./list.api";
+
+export const fetchList = () => dispatch => 
+  listApi.fetchList()
+  .then(payload => dispatch({type: 'fetchList', payload}));
+
+export const addListItem = () => dispatch => {
+  listApi.addListItem()
+  .then(payload => dispatch({type: 'addListItem', payload}));
+}
+
+export const closeListItem = (number) => dispatch => 
+  listApi.closeListItem(number)
+  .then(item => ({
+    state: item.state,
+    id: item.id,
+  }))
+  .then(payload => dispatch({type: 'closeListItem', payload}));
