59ada1643bb3801ee2a4d724b81f18e1129bc254
Lesha <dudaaleksey@gmail.com>
2017-07-21 12:05:46 +0300

    Refactor

--- classworks/lesha-duda/10_redux/simplified_github_issues/src/components/list/list.js
+++ classworks/lesha-duda/10_redux/simplified_github_issues/src/components/list/list.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 import MyButton from '../button/button.js';
 import fromStore from '../../resourses/list.selectors';
