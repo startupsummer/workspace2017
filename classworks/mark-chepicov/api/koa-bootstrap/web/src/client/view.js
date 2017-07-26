31b650c260a69a26e9f0103c805a97e15fd2540a
Chepicov Mark <chepicov.mark@mail.ru>
2017-07-26 21:13:12 +0300

    mc_API_12

---
+++ classworks/mark-chepicov/api/koa-bootstrap/web/src/client/view.js
@@ -0,0 +1,53 @@
+import { post } from './api.client';
+
+const $ = require('jquery');
+
+/* eslint-disable */
+
+document.forms.summer.onsubmit = (event) => {
+  event.preventDefault();
+  const form = document.forms.summer;
+  post('post-form', null, {
+    email: form.email.value,
+    password: form.password.value,
+  })
+  .then((res) => {
+    console.dir(res);
+    form.style.background = 'none';
+    $('.main__text').html('Everything is ok');
+    document.querySelector('#forma').style.display = 'none';
+    document.querySelector('#login').style.display = 'block';
+    if (res.token) {
+      localStorage.setItem('token', res.token);
+    }
+  })
+  .catch(() => {
+    form.style.background = 'red';
+    $('.main__text').html('Wrong email or password');
+  });
+};
+
+document.querySelector('#login').onclick = (event) => {
+  post('access', null, {
+    token: localStorage.getItem('token'),
+  })
+  .then((res) => {
+    event.target.style.background = 'green';
+    $('.main__text').html('SECRET INFORMATION!!!!!!!!');
+    document.querySelector('#logout').style.display = 'block';
+  })
+  .catch(() => {
+    event.target.style.background = 'red';
+    $('.main__text').html('Something went wrong :/');
+  });
+};
+
+
+document.querySelector('#logout').onclick = (event) => {
+  event.target.style.display = 'none';
+  $('.main__text').html('Log in');
+  document.querySelector('#login').style.background = 'white';
+  document.querySelector('#login').style.display = 'none';
+  document.querySelector('#forma').style.display = 'flex';
+  localStorage.clear();
+};
