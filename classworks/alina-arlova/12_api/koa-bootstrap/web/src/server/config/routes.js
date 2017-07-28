a1f393c241bfcb710023b804a5f3487ea27ff121
arlovaalina <arlovaalina@gmail.com>
2017-07-28 15:12:20 +0300

    Add token generation

--- classworks/alina-arlova/12_api/koa-bootstrap/web/src/server/config/routes.js
+++ classworks/alina-arlova/12_api/koa-bootstrap/web/src/server/config/routes.js
@@ -1,7 +1,9 @@
 const router = require('koa-router')();
-const auth = require('./authentication.js');
+const jwt = require('jsonwebtoken');
+const hash = require('password-hash');
+// const auth = require('./authentication.js');
 
-const users = [{login: 'a'}];
+const users = [];
 
 router.get('/hello', async (ctx) => {
   ctx.session.count = ctx.session.count ? ctx.session.count + 1 : 1;
@@ -15,6 +17,9 @@ router.post('/form', async (ctx) => {
   ctx.checkBody('login', 'Inappropriate length').notEmpty();
   ctx.checkBody('password', 'Inappropriate length').notEmpty();
 
+  let login = ctx.request.body.login;
+  let password = ctx.request.body.password;
+
   if(ctx.validationErrors()) {
     ctx.status = 400;
     ctx.body = ctx.validationErrors();
@@ -22,18 +27,34 @@ router.post('/form', async (ctx) => {
     console.dir(ctx.request.body)
     ctx.body = { OK: 'true' }
     // auth.authenticate(ctx.request.body.login, ctx.request.body.password);
-    if (authenticate(ctx.request.body.login, ctx.request.body.password)) {
-      console.log('You are successfully authenticate');
-    } else {
-      console.log('Your info is not correct');
-    }
+    signIn(login, password);
   }
 
 });
 
-function authenticate(login, password) {
-  const filteredUsers = users.filter((user) => {return user.login === login});
-  return filteredUsers.length;
+function signIn(login, password) {
+  const sameUser = users.filter((user) => {
+    return (user.login === login && hash.verify(password, user.passwordHash))
+  });
+
+  if (sameUser.length) {
+    console.log('You are already signed in');
+  } else {
+    const sameLogin = users.filter((user) => {
+      return (user.login === login)
+    });
+
+    if (sameLogin.length) {
+      console.log('The user with a such login already exists');
+    } else {
+      const passwordHash = hash.generate(password);
+      users.push({login, passwordHash});
+      const token = jwt.sign({login}, 'secretMessage');
+      console.log('TOKEN ', token);
+      console.log('You are successfully authenticated');
+    }
+  }
+  console.log(users);
 }
 
 module.exports = router.routes();
