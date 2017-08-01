e16b3c15032ee2882fa29043ac7df2abdc6aa1d2
Sergey Oliferchik <oliferchik_s_n@outlook.com>
2017-08-01 18:42:31 +0300

    11koa_mongo_1/ add koa webpack middleware

--- classworks/sergei-oliferchik/11_koa/web/src/server/config/koa.js
+++ classworks/sergei-oliferchik/11_koa/web/src/server/config/koa.js
@@ -1,33 +1,31 @@
-const config = require('web/config');
-const path = require('path');
-const session = require('koa-generic-session');
-const redisStore = require('koa-redis');
-const views = require('koa-views');
-const routes = require('./routes');
-const logger = require('koa-logger');
-const handlebars = require('handlebars');
-const serve = require('koa-static');
+const config = require('web/config')
+const path = require('path')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const views = require('koa-views')
+const routes = require('./routes')
+const logger = require('koa-logger')
+const handlebars = require('handlebars')
+const serve = require('koa-static')
 
-const { devMiddleware } = require("web/koa-webpack-middleware-master/middleware");
-const webpack = require('webpack');
-const webpackConfig = require('web/webpack.config');
-const koaBody = require('koa-body');
+const { devMiddleware } = require("web/koa-webpack-middleware-master/middleware")
+const webpack = require('webpack')
+const webpackConfig = require('web/webpack.config')
+const koaBody = require('koa-body')
 
-handlebars.registerHelper('json', context => JSON.stringify(context));
+handlebars.registerHelper('json', context => JSON.stringify(context))
 
 module.exports = (app) => {
-  require('koa-validate')(app);
-  
-  app.use(koaBody());
+  require('koa-validate')(app)
 
-  app.use(serve(path.join(__dirname, './../../client')))
+  app.use(koaBody())
 
   app.use(views(path.join(__dirname, './../../client'), {
     default: 'html',
     map: { html: 'handlebars' },
-  }));
+  }))
 
-  app.use(devMiddleware(webpack(webpackConfig)));
+  app.use(devMiddleware(webpack(webpackConfig)))
 
   app.use(session({
     store: redisStore({
@@ -38,20 +36,22 @@ module.exports = (app) => {
     cookie: {
       expires: false,
     },
-  }));
+  }))
 
-  app.use(logger());
+  app.use(logger())
 
   app.use(async (ctx, next) => {
     try {
-      await next();
+      await next()
     } catch (err) {
-      console.log(err);
-      ctx.status = err.status || 500;
-      ctx.body = err.message;
-      ctx.app.emit('error', err, ctx);
+      console.log(err)
+      ctx.status = err.status || 500
+      ctx.body = err.message
+      ctx.app.emit('error', err, ctx)
     }
-  });
+  })
 
-  app.use(routes);
-};
+    app.use(routes)
+    
+    app.use(serve(path.join(__dirname, './../../client')))
+}
