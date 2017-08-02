da1e7973ed09b45317a9e592c65670490886e818
Ilya <i.kohanvs@gmail.com>
2017-08-02 09:25:54 +0300

    delete unnecessary files, delete unnecessary imports

--- classworks/ilya-koanovskiy/14_api_testing/api/src/tests/app.spec.js
+++ classworks/ilya-koanovskiy/14_api_testing/api/src/tests/app.spec.js
@@ -5,14 +5,10 @@ mockAmazon.setAmazonMock();
 const supertest = require('supertest');
 const app = require('../app')
 const request = supertest.agent(app.listen())
-const auth = require('./resources/auth')
 const chai = require('chai')
 const userTests = require('./resources/staff')
 const taskTests = require('./resources/task')
 const amazonTests = require('./resources/amazon')
-const userFactory = require('./resources/staff/user.factory.js')
-const taskFactory = require('./resources/task/task.factory.js')
-
 
 chai.should();
 
